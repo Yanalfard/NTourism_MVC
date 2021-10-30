ad476d019c30bc9be09797088adfa6ec22b09ca8
yanal <yanal.fard@gmail.com>
2021-10-30 23:25:46 +0330

    vcx

--- NTourism/Utilities/Config.cs
+++ NTourism/Utilities/Config.cs
@@ -3,7 +3,8 @@
     public static class Config
     {
         //public static readonly string ConnectionString = "Data Source=103.216.62.27;Initial Catalog=rimatrip_NTourism;User ID=rimatrip_root;Password=1710ahmad.fard";
-        public static readonly string ConnectionString = "Data Source=103.216.62.27;Initial Catalog=rimatrip_NTourism;User ID=Yanal;Password=1710ahmad.fard";
+        public static readonly string ConnectionString = "Data Source=185.55.224.183;Initial Catalog=asamedc1_rimatrip;User ID=asamedc1_Yanal4;Password=2fjS9CYVYkgS5V8";
+        //public static readonly string ConnectionString = "Data Source=103.216.62.27;Initial Catalog=rimatrip_NTourism;User ID=Yanal;Password=1710ahmad.fard";
         //public static readonly string ConnectionString = "Data Source=.;Initial Catalog=NTourism;Integrated Security=True";
     }
 }
